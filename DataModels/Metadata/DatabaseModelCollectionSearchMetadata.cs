2e34911e37af88df9afbaab7748a0a15fa1e978b
Brian Weiss <brian_weiss@hotmail.com>
2014-10-19 12:03:22 -0600

    add DatabaseModelCollectionSearchMetadata

---
+++ DataModels/Metadata/DatabaseModelCollectionSearchMetadata.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Jamiras.Database;
+
+namespace Jamiras.DataModels.Metadata
+{
+    public abstract class DatabaseModelCollectionSearchMetadata<T> : DatabaseModelCollectionMetadata<T>
+        where T : DataModelBase, new()
+    {
+        protected DatabaseModelCollectionSearchMetadata(ModelProperty searchProperty, SearchType searchType)
+        {
+            if (searchType == SearchType.None)
+                throw new ArgumentException("searchType");
+
+            var metadata = RelatedMetadata.GetFieldMetadata(searchProperty) as StringFieldMetadata;
+            if (metadata == null)
+                throw new ArgumentException("searchProperty does not map to a string field", "searchProperty");
+
+            _searchField = metadata.FieldName;
+            _searchType = searchType;
+        }
+
+        protected enum SearchType
+        {
+            None = 0,
+            Exact,
+            StartsWith,
+            EndsWith,
+            Contains
+        }
+
+        private readonly string _searchField;
+        private readonly SearchType _searchType;
+
+        public override sealed bool Query(ModelBase model, object primaryKey, IDatabase database)
+        {
+            string searchText = primaryKey.ToString();
+            switch (_searchType)
+            {
+                case SearchType.Contains:
+                    searchText = '%' + searchText + '%';
+                    break;
+                case SearchType.StartsWith:
+                    searchText = searchText + '%';
+                    break;
+                case SearchType.EndsWith:
+                    searchText = '%' + searchText;
+                    break;
+            }
+
+            return base.Query(model, searchText, database);
+        }
+
+        protected override void CustomizeQuery(ModelQueryExpression queryExpression)
+        {
+            if (_searchType == SearchType.Exact)
+                queryExpression.AddFilter(_searchField, FilterValueToken);
+            else
+                queryExpression.AddLikeFilter(_searchField, FilterValueToken);
+
+            base.CustomizeQuery(queryExpression);
+        }
+    }
+}
