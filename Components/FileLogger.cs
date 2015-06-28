2a44338e4c439e1c19dd73ea17e4a0aa893bd1d3
Brian Weiss <brian_weiss@hotmail.com>
2015-06-28 09:48:13 -0600

    add FileLogger

---
+++ Components/FileLogger.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Jamiras.Components
+{
+    public class FileLogger : ILogTarget
+    {
+        public FileLogger(string filename)
+        {
+            _stream = File.CreateText(filename);
+            _stream.AutoFlush = true;
+        }
+
+        private readonly StreamWriter _stream;
+
+        public void Write(string message)
+        {
+            _stream.WriteLine(message);
+        }
+    }
+}
