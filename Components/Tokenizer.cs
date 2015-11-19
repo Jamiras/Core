f2fcc32478e57ab477efca48c35fb054c0590cea
bweiss <brian.weiss@oracle.com>
2015-11-19 15:13:25 -0700

    add GraphQuery and Json parsers

---
+++ Components/Tokenizer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Jamiras.Components
+{
+    public class Tokenizer
+    {
+        public Tokenizer(string input)
+            : this(new MemoryStream(Encoding.UTF8.GetBytes(input)))
+        {
+        }
+
+        public Tokenizer(Stream input)
+        {
+            _stream = input;
+            Advance();
+        }
+
+        private readonly Stream _stream;
+
+        /// <summary>
+        /// Gets the next character in the stream.
+        /// </summary>
+        public Char NextChar { get; private set; }
+
+        /// <summary>
+        /// Advances to the next character in the stream.
+        /// </summary>
+        public void Advance()
+        {
+            var b = _stream.ReadByte();
+            if (b < 0)
+            {
+                NextChar = (char)0x00;
+            }
+            else if (b < 0x80)
+            {
+                NextChar = (char)b;
+            }
+            else if (b < 0xC0)
+            {
+                NextChar = (char)0xFFFD;
+            }
+            else if (b < 0xE0)
+            {
+                var b2 = _stream.ReadByte();
+                NextChar = (char)((b << 5) | (b2 & 0x1F));
+            }
+            else if (b < 0xF0)
+            {
+                var b2 = _stream.ReadByte();
+                var b3 = _stream.ReadByte();
+                NextChar = (char)((b << 10) | ((b2 & 0x1F) << 5) | (b3 & 0x1F));
+            }
+            else
+            {
+                NextChar = (char)0xFFFD;
+            }
+        }
+
+        /// <summary>
+        /// Advances to the next non-whitespace character in the stream.
+        /// </summary>
+        public void SkipWhitespace()
+        {
+            while (Char.IsWhiteSpace(NextChar))
+                Advance();
+        }
+
+        /// <summary>
+        /// Matches a token containing alphanumeric characters and/or underscores, or a quoted string.
+        /// </summary>
+        public string ReadValue()
+        {
+            if (NextChar == '"') 
+                return ReadQuotedString();
+            
+            if (Char.IsDigit(NextChar))
+                return ReadNumber();
+
+            return ReadIdentifier();
+        }
+
+        /// <summary>
+        /// Matches a token containing alphanumeric characters and/or underscores.
+        /// </summary>
+        public string ReadIdentifier()
+        {
+            var builder = new StringBuilder();
+            if (!Char.IsDigit(NextChar))
+            {
+                while (Char.IsLetterOrDigit(NextChar) || NextChar == '_')
+                {
+                    builder.Append(NextChar);
+                    Advance();
+                }
+            }
+
+            return (builder.Length == 0) ? String.Empty : builder.ToString();
+        }
+
+        /// <summary>
+        /// Matches a token containing numeric characters, possibly with a single decimal separator.
+        /// </summary>
+        public string ReadNumber()
+        {
+            if (!Char.IsDigit(NextChar))
+                return String.Empty;
+
+            var builder = new StringBuilder();
+            while (Char.IsDigit(NextChar))
+            {
+                builder.Append(NextChar);
+                Advance();
+            }
+
+            if (NextChar == '.')
+            {
+                builder.Append('.');
+                Advance();
+
+                while (Char.IsDigit(NextChar))
+                {
+                    builder.Append(NextChar);
+                    Advance();
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Matches a quoted string.
+        /// </summary>
+        public String ReadQuotedString()
+        {
+            if (NextChar != '"')
+                throw new InvalidOperationException("expecting quote, found " + NextChar);
+
+            Advance();
+            var builder = new StringBuilder();
+            while (NextChar != '"')
+            {
+                if (NextChar == '\\')
+                {
+                    Advance();
+                    switch (NextChar)
+                    {
+                        case 't':
+                            builder.Append("\t");
+                            Advance();
+                            continue;
+                        case 'n':
+                            builder.AppendLine();
+                            Advance();
+                            continue;
+                    }
+                }
+                else if (NextChar == 0)
+                {
+                    throw new InvalidOperationException("closing quote not found for quoted string");
+                }
+
+                builder.Append(NextChar);
+                Advance();
+            }
+
+            Advance();
+            return (builder.Length == 0) ? String.Empty : builder.ToString();
+        }
+    }
+}
---
+++ Tests/Components/TokenizerTests.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Jamiras.Components;
+using NUnit.Framework;
+
+namespace Jamiras.Core.Tests.Components
+{
+    [TestFixture]
+    public class TokenizerTests
+    {
+        private Tokenizer CreateTokenizer(string input)
+        {
+            return new Tokenizer(new MemoryStream(Encoding.UTF8.GetBytes(input)));
+        }
+
+        [Test]
+        public void TestEmptyString()
+        {
+            var tokenizer = CreateTokenizer("");
+            Assert.That(tokenizer.NextChar, Is.EqualTo((char)0));
+        }
+
+        [Test]
+        public void TestAdvance()
+        {
+            var tokenizer = CreateTokenizer("happy");
+            Assert.That(tokenizer.NextChar, Is.EqualTo('h'));
+            tokenizer.Advance();
+            Assert.That(tokenizer.NextChar, Is.EqualTo('a'));
+            tokenizer.Advance();
+            Assert.That(tokenizer.NextChar, Is.EqualTo('p'));
+            tokenizer.Advance();
+            Assert.That(tokenizer.NextChar, Is.EqualTo('p'));
+            tokenizer.Advance();
+            Assert.That(tokenizer.NextChar, Is.EqualTo('y'));
+            tokenizer.Advance();
+            Assert.That(tokenizer.NextChar, Is.EqualTo((char)0));
+            tokenizer.Advance();
+            Assert.That(tokenizer.NextChar, Is.EqualTo((char)0));
+        }
+
+        [Test]
+        public void TestSkipWhitespaceEmptyString()
+        {
+            var tokenizer = CreateTokenizer("");
+            tokenizer.SkipWhitespace();
+            Assert.That(tokenizer.NextChar, Is.EqualTo((char)0));            
+        }
+
+        [Test]
+        public void TestSkipWhitespaceNoWhitespace()
+        {
+            var tokenizer = CreateTokenizer("happy");
+            tokenizer.SkipWhitespace();
+            Assert.That(tokenizer.NextChar, Is.EqualTo('h'));
+        }
+
+        [Test]
+        public void TestSkipWhitespaceSpaces()
+        {
+            var tokenizer = CreateTokenizer("   happy");
+            tokenizer.SkipWhitespace();
+            Assert.That(tokenizer.NextChar, Is.EqualTo('h'));
+        }
+
+        [Test]
+        public void TestSkipWhitespaceMixed()
+        {
+            var tokenizer = CreateTokenizer(" \t   \r\n   happy");
+            tokenizer.SkipWhitespace();
+            Assert.That(tokenizer.NextChar, Is.EqualTo('h'));
+        }
+
+        [Test]
+        public void TestReadIdentifierPascalCase()
+        {
+            var tokenizer = CreateTokenizer("RedSquare2();");
+            var token = tokenizer.ReadIdentifier();
+            Assert.That(token, Is.EqualTo("RedSquare2"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('('));
+        }
+
+        [Test]
+        public void TestReadIdentifierCamelCase()
+        {
+            var tokenizer = CreateTokenizer("redSquare2();");
+            var token = tokenizer.ReadIdentifier();
+            Assert.That(token, Is.EqualTo("redSquare2"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('('));
+        }
+
+        [Test]
+        public void TestReadIdentifierAllCaps()
+        {
+            var tokenizer = CreateTokenizer("RED_SQUARE2();");
+            var token = tokenizer.ReadIdentifier();
+            Assert.That(token, Is.EqualTo("RED_SQUARE2"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('('));
+        }
+
+        [Test]
+        public void TestReadIdentifierLeadingUnderscore()
+        {
+            var tokenizer = CreateTokenizer("_field = 6;");
+            var token = tokenizer.ReadIdentifier();
+            Assert.That(token, Is.EqualTo("_field"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(' '));
+        }
+
+        [Test]
+        public void TestReadIdentifierNumber()
+        {
+            var tokenizer = CreateTokenizer("6");
+            var token = tokenizer.ReadIdentifier();
+            Assert.That(token, Is.EqualTo(""));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('6'));
+        }
+
+        [Test]
+        public void TestReadNumberInteger()
+        {
+            var tokenizer = CreateTokenizer("16;");
+            var token = tokenizer.ReadNumber();
+            Assert.That(token, Is.EqualTo("16"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadNumberWord()
+        {
+            var tokenizer = CreateTokenizer("happy");
+            var token = tokenizer.ReadNumber();
+            Assert.That(token, Is.EqualTo(""));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('h'));
+        }
+
+        [Test]
+        public void TestReadNumberFloat()
+        {
+            var tokenizer = CreateTokenizer("16.773;");
+            var token = tokenizer.ReadNumber();
+            Assert.That(token, Is.EqualTo("16.773"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadNumberPhone()
+        {
+            var tokenizer = CreateTokenizer("406.555.1234");
+            var token = tokenizer.ReadNumber();
+            Assert.That(token, Is.EqualTo("406.555"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('.'));
+        }
+
+        [Test]
+        public void TestReadNumberDate()
+        {
+            var tokenizer = CreateTokenizer("12/25/2000");
+            var token = tokenizer.ReadNumber();
+            Assert.That(token, Is.EqualTo("12"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('/'));
+        }
+
+        [Test]
+        public void TestReadNumberLeadingPeriod()
+        {
+            var tokenizer = CreateTokenizer(".6");
+            var token = tokenizer.ReadNumber();
+            Assert.That(token, Is.EqualTo(""));
+            Assert.That(tokenizer.NextChar, Is.EqualTo('.'));
+        }
+
+        [Test]
+        public void TestReadQuotedStringNotQuoted()
+        {
+            var tokenizer = CreateTokenizer("happy");
+            Assert.That(() => tokenizer.ReadQuotedString(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void TestReadQuotedStringUnclosed()
+        {
+            var tokenizer = CreateTokenizer("\"happy");
+            Assert.That(() => tokenizer.ReadQuotedString(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void TestReadQuotedStringSimple()
+        {
+            var tokenizer = CreateTokenizer("\"happy\";");
+            var token = tokenizer.ReadQuotedString();
+            Assert.That(token, Is.EqualTo("happy"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadQuotedStringSentence()
+        {
+            var tokenizer = CreateTokenizer("\"I am happy.\";");
+            var token = tokenizer.ReadQuotedString();
+            Assert.That(token, Is.EqualTo("I am happy."));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadQuotedStringEscapedQuotes()
+        {
+            var tokenizer = CreateTokenizer("\"I am \\\"happy\\\".\";");
+            var token = tokenizer.ReadQuotedString();
+            Assert.That(token, Is.EqualTo("I am \"happy\"."));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadQuotedStringEscapedTab()
+        {
+            var tokenizer = CreateTokenizer("\"I am \\thappy.\";");
+            var token = tokenizer.ReadQuotedString();
+            Assert.That(token, Is.EqualTo("I am \thappy."));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadQuotedStringEscapedNewLine()
+        {
+            var tokenizer = CreateTokenizer("\"I am \\nhappy.\";");
+            var token = tokenizer.ReadQuotedString();
+            Assert.That(token, Is.EqualTo("I am \r\nhappy."));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadQuotedStringEmpty()
+        {
+            var tokenizer = CreateTokenizer("\"\";");
+            var token = tokenizer.ReadQuotedString();
+            Assert.That(token, Is.EqualTo(""));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadValueNumber()
+        {
+            var tokenizer = CreateTokenizer("6.8;");
+            var token = tokenizer.ReadValue();
+            Assert.That(token, Is.EqualTo("6.8"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadValueQuotedString()
+        {
+            var tokenizer = CreateTokenizer("\"happy\";");
+            var token = tokenizer.ReadValue();
+            Assert.That(token, Is.EqualTo("happy"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadValueVariable()
+        {
+            var tokenizer = CreateTokenizer("happy;");
+            var token = tokenizer.ReadValue();
+            Assert.That(token, Is.EqualTo("happy"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+
+        [Test]
+        public void TestReadValueField()
+        {
+            var tokenizer = CreateTokenizer("_happy;");
+            var token = tokenizer.ReadValue();
+            Assert.That(token, Is.EqualTo("_happy"));
+            Assert.That(tokenizer.NextChar, Is.EqualTo(';'));
+        }
+    }
+}
