9b6643fb32fea8b3cc95989914252aca23b59c3a
Brian Weiss <brian_weiss@hotmail.com>
2019-07-23 19:44:24 -0600

    prep for consuming Core as a submodule

--- DownloadLibs.bat
+++ DownloadLibs.bat
@@ -5,14 +5,26 @@ set MOQ_VERSION=4.3
 set NUNIT_VERSION=3.11
 set DOTNET_VERSION=40
 
+set LIB_DIR=%1
+if "%LIB_DIR%" == "" set LIB_DIR=../lib
+
+rem ==== Check parallel folder first ====
 set LIBS_EXIST=1
-set MOQ_LIB=../lib/moq-%MOQ_VERSION%/net%DOTNET_VERSION%/Moq.dll
-set NUNIT_LIB=../lib/nUnit-%NUNIT_VERSION%/net%DOTNET_VERSION%/nunit.framework.dll
+set MOQ_LIB=%LIB_DIR%/../../libs/moq-%MOQ_VERSION%/net%DOTNET_VERSION%/Moq.dll
+set NUNIT_LIB=%LIB_DIR%/../../libs/nUnit-%NUNIT_VERSION%/net%DOTNET_VERSION%/nunit.framework.dll
 if not exist %MOQ_LIB% set LIBS_EXIST=0
 if not exist %NUNIT_LIB% set LIBS_EXIST=0
-
 if "%LIBS_EXIST%" == "1" goto done
 
+rem ==== Check nested folder ====
+set LIBS_EXIST=1
+set MOQ_LIB=%LIB_DIR%/moq-%MOQ_VERSION%/net%DOTNET_VERSION%/Moq.dll
+set NUNIT_LIB=%LIB_DIR%/nUnit-%NUNIT_VERSION%/net%DOTNET_VERSION%/nunit.framework.dll
+if not exist %MOQ_LIB% set LIBS_EXIST=0
+if not exist %NUNIT_LIB% set LIBS_EXIST=0
+if "%LIBS_EXIST%" == "1" goto done
+
+rem ==== Download package ====
 if not exist package.zip (
     echo Downloading %PACKAGE%...
     powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest %PACKAGE% -OutFile package.zip"
@@ -22,9 +34,11 @@ if not exist package.zip (
     )
 )
 
-echo Extracting libs...
-powershell -Command "Expand-Archive 'package.zip' '../lib'"
+rem ==== Extract package ====
+echo Extracting to %LIB_DIR%...
+powershell -Command "Expand-Archive 'package.zip' '%LIB_DIR%'"
 
+rem ==== Validate package ====
 set LIBS_EXIST=1
 if not exist %MOQ_LIB% set LIBS_EXIST=0
 if not exist %NUNIT_LIB% set LIBS_EXIST=0
@@ -34,6 +48,7 @@ if "%LIBS_EXIST%" == "0" (
     goto done
 )
 
+rem ==== Cleanup ====
 del package.zip
 
 echo Libs installed
